22f7475b22d9409ab02789f98e5040f292c305b0
Lokesh350 <lokeshrajagopal2@gmail.com>
2024-05-22 14:23:51 +0530

    before_install.sh

---
+++ before_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Example script for tasks to be executed before installing the application
+
+echo "BeforeInstall: Running pre-installation tasks..."
+# Add your pre-installation tasks here
