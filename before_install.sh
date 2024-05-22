bee8a8d0ca7ca90b304538ac8019251b35b57e8e
Lokesh350 <lokeshrajagopal2@gmail.com>
2024-05-22 14:33:56 +0530

    before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Example script for tasks to be executed before installing the application
-
-echo "BeforeInstall: Running pre-installation tasks..."
-# Add your pre-installation tasks here
+# Example: Stop a service
+service my-service stop
