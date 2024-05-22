0bad13cdfbfb0869b376000a9e187ecb655f6e17
Lokesh350 <lokeshrajagopal2@gmail.com>
2024-05-22 14:24:41 +0530

    after_install.sh

---
+++ after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Example script for tasks to be executed after installing the application
+
+echo "AfterInstall: Running post-installation tasks..."
+# Add your post-installation tasks here
